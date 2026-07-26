53cc003588591d746b34f020e078680cf1bdb2ee
Pfoetiman76 <Pfoetiman76@hotmail.com>
2026-07-26 23:32:00 +0200

    update pitwall.py

--- client/pitwall.py
+++ client/pitwall.py
@@ -2,15 +2,16 @@
 
 Ohne Argumente oeffnet sich das Fahrer-Fenster.
 Mit --run-client laeuft der Client im Hintergrund (so ruft das Fenster
-sich selbst als eigenen Prozess auf - noetig, damit aus allem eine
+sich selbst als eigenen Prozess auf - poetig, damit aus allem eine
 einzige .exe werden kann).
 """
+
 import sys
+import gui
+import run_client
 
 if "--run-client" in sys.argv:
     sys.argv.remove("--run-client")
-    import run_client
-    sys.exit(run_client.main())
+    run_client.main()
 else:
-    import gui
     gui.main()
